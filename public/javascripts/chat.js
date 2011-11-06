5b96938cff974f28fca8fbea746fd61eb5816783
Adrien Jarthon <contact@adrienjarthon.com>
2011-11-06 07:19:09 +0100

    chat design

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -5,33 +5,14 @@ var chat_load = function(url) {
   var messages = $("ul.messages");
   if (messages.length)
   {
-    var status = $("<div class=\"status sync\">Connecting to push server</div>");
-    command.prepend(status);
-    var timer;
     var Socket = "MozWebSocket" in window ? MozWebSocket : WebSocket;
     var ws = new Socket("ws://" + window.location.hostname + ":8080/" + command.data("filters"));
     ws.onmessage = function(evt) { 
-      status.text("Syncing...");
-      status.addClass("sync");
-      if (timer) {
-        clearTimeout(timer);
-        timer = null
-      }
-      timer = setTimeout(function() {
-        status.removeClass("sync");
-        var d = new Date;
-        status.text("Synced at " + d.toLocaleTimeString());
-      }, 2000)
       messages.append($("<li>" + evt.data + "</li>"));
     };
     ws.onopen = function() {
-      status.text("Connected to push server");
-      status.removeClass("sync");
     };
     ws.onclose = function(e) {
-      status.text("Can't connect to push server");
-      status.removeClass("sync")
-      status.addClass("error")    
     }
     form.submit(function() {
       ws.send(field.val());
