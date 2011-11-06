6ec8353a7a894134abf5334741c0ee63f83602c4
arno <arno@arno-1215N.(none)>
2011-11-06 09:09:31 +0100

    Search mathodes

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -12,30 +12,6 @@ class SearchController < ApplicationController
   def chat
     render :chat, :layout => nil if request.xhr?
   end
-  
-protected
-  def login_required?
-    redirect_to new_session_path if @user.nil?
-  end
-
-  def parse
-    @filters = []
-    @path = params[:path]
-    @last = "User"
-    @origin = @user 
-    return if not @path
-    @path = @path.split('/')
-    while @path.any?
-      filter, arg = @path.shift 2
-      link = LINKS[filter]
-      next if link.nil?
-      @filters += [filter, arg]
-      if @path.empty?
-        @last = link['target']
-        @origin = @last.constantize.find_by_permalink(arg)
-      end
-    end
-  end
 
   def listened
     name = params[:arg]
@@ -63,5 +39,29 @@ protected
     end
     track = Track.from_rockstar rt
     render :partial => 'suggestion', :locals => {:link => LINKS['fan'], :target => track}    
+  end
+  
+protected
+  def login_required?
+    redirect_to new_session_path if @user.nil?
+  end
+
+  def parse
+    @filters = []
+    @path = params[:path]
+    @last = "User"
+    @origin = @user 
+    return if not @path
+    @path = @path.split('/')
+    while @path.any?
+      filter, arg = @path.shift 2
+      link = LINKS[filter]
+      next if link.nil?
+      @filters += [filter, arg]
+      if @path.empty?
+        @last = link['target']
+        @origin = @last.constantize.find_by_permalink(arg)
+      end
+    end
   end
 end
